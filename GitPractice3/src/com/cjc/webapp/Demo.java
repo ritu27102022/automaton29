39e246957fafe8584e06b9a400ec4cf3bae8b77c
LENOVO <LENOVO@DESKTOP-NMF9K3U>
2025-03-29 15:58:53 +0530

    by eclipse to Git transfer

---
+++ GitPractice3/src/com/cjc/webapp/Demo.java
@@ -0,0 +1,29 @@
+package com.cjc.webapp;
+
+public class Demo 
+{
+	public void m1()
+	{
+		System.out.println("This is m1 method");
+	}
+	
+	public void m2()
+	{
+		System.out.println("This is m2 method");
+	}
+
+	public void m3()
+	{
+		System.out.println("This is m3 method");
+	}
+
+	public static void main(String[] args) 
+	{
+	
+		
+		
+			System.out.println("this is m4 method");
+		
+	}
+
+}
